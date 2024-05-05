4b65124b764d6456260519b5817a9161d5592dba
skillshifting <167896191+skillshifting@users.noreply.github.com>
2024-05-05 13:49:52 +0300

    Add files via upload

---
+++ triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+	int n;
+	std::cout << "print n ";
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n - i; j++) {
+			std::cout << " ";
+		}
+		for (int j = 1; j <= 2 * i - 1; j++) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
